b9b361a6985d9bb44381b0ccaabaeed9b5ec69bf
Daniele Del Piano <daniele.delpiano30@gmail.com>
2022-10-21 18:12:05 +0100

    added the common classes

---
+++ src/app/common/purchase.ts
@@ -0,0 +1,11 @@
+import { Order } from './order';
+import { Address } from './address';
+import { Customer } from './customer';
+import { OrderItem } from './order-item';
+export class Purchase {
+  customer?: Customer;
+  shippingAddress?: Address;
+  billingAddress?: Address;
+  order?: Order;
+  orderItems?: OrderItem[];
+}

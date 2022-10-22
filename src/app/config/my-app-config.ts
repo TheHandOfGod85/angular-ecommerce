a68cde639e381fa9317c0dc7ed24423ec38ca505
Daniele Del Piano <daniele.delpiano30@gmail.com>
2022-10-22 16:30:59 +0100

    added okta config file and installed the dependencies

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oa6yvk16vk4bWQCF5d7',
+    issuer: 'https://dev-41694973.okta.com/oath2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+  },
+};
